15c46d60e2b9c97617acee654d88741e688ecdf7
zaid sayyyed <zaidsayyyed3108@gmail.com>
2026-07-14 02:39:34 +0530

    fix: Clear Turbopack cache, fix Dark Reader hydration mismatch on body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       className={`${inter.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
         <SmoothScrolling>
           {children}
         </SmoothScrolling>

7c6738fd37826e407d4e74000179ccf58c470fd5
zaid sayyyed <zaidsayyyed3108@gmail.com>
2026-07-14 02:07:19 +0530

    fix: Add suppressHydrationWarning to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col font-sans">
         <SmoothScrolling>
